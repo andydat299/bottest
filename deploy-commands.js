aaab901f1f1fa5d7ea7252cad647ca953fa475c6
Trần Vĩnh Đạt <datbautroi1@gmail.com>
2025-08-06 21:17:52 +0700

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,5 +1,6 @@
 import 'dotenv/config';
-import { REST, Routes } from 'discord.js';
+import { REST } from '@discordjs/rest';
+import { Routes } from 'discord-api-types/v9';
 import fs from 'fs';
 
 const commands = [];
